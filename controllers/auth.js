264c8d490d1ca91101583c10849f586931a7f4f7
KamilCetiner <cetiner.kamil84@gmail.com>
2020-12-26 21:08:51 +0100

    model user created

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,24 @@
-const register = (req, res, next) => {
+const User = require("../models/Users")
+
+const register = async (req, res, next) => {
+
+    const name = "Kemal";
+    const email = "kemal@gmail.com";
+    const password = "123456";
+
+    //async, await
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
     res
     .status(200)
     .json({
-        success : true
+        success : true,
+        data : user
     })
 };
 
